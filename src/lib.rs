f93afc428a1be45634f8a6927924fce6db792965
Nicholas Warfield <warfield9000@gmail.com>
2020-01-08 15:56:00 -0800

    Remove Entity Works Entities can now be 'removed'... needs refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -10,20 +10,32 @@ pub struct Velocity(f32, f32);
 #[derive(Debug)]
 pub struct Entity
 {
+	pub generation: usize,
 	pub index: usize,
 }
 
 
 // name this better, it's accurate but long
-#[derive(Default, Debug)]
+#[derive(Debug)]
 pub struct EntityComponentSystem<'a>
 {
+	generation: usize,
 	names: Vec<Name<'a>>,
 	positions: Vec<Position>,
 	velocities: Vec<Velocity>,
 }
+
 impl<'a> EntityComponentSystem<'a>
 {
+	pub fn new() -> EntityComponentSystem<'a>
+	{
+		EntityComponentSystem {
+			generation: 1,
+			names: vec![],
+			positions: vec![],
+			velocities: vec![],
+		}
+	}
 	pub fn create_entity(
 		&mut self,
 		name: &'a str, 
@@ -34,15 +46,31 @@ impl<'a> EntityComponentSystem<'a>
 		self.names.push(Name(name));
 		self.positions.push(position);
 		self.velocities.push(velocity);
-		Entity { index: self.names.len() - 1 }
+		Entity {
+			generation: self.generation,
+			index: self.names.len() - 1
+		}
 	}
-	pub fn get(&self, ent: &Entity)
-		-> (&'a Name, &'a Position, &'a Velocity)
+
+	pub fn remove_entity(&mut self, ent: &mut Entity)
 	{
-		(&self.names[ent.index],
-		 &self.positions[ent.index],
-		 &self.velocities[ent.index],
-		 )
+		ent.generation = 0;
+	}
+
+	pub fn get(&self, ent: &Entity)
+		-> Option<(&'a Name, &'a Position, &'a Velocity)>
+	{
+		if ent.generation != self.generation
+		{
+			None
+		}
+		else
+		{
+			Some((&self.names[ent.index],
+			 &self.positions[ent.index],
+			 &self.velocities[ent.index],
+			 ))
+		}
 	}
 }
 
@@ -54,7 +82,7 @@ pub mod tests
 	#[test]
 	fn create_entities()
 	{
-		let mut ecs: EntityComponentSystem = Default::default();
+		let mut ecs = EntityComponentSystem::new();
 		let e1 = ecs.create_entity(
 			"Pilot Pete",
 			Position(5.0, 5.0),
@@ -66,14 +94,44 @@ pub mod tests
 			Velocity(0.0, -10.0),
 			);
 
-		let (name, pos, vel) = ecs.get(&e1);
-		assert_eq!(Name("Pilot Pete"), *name);
-		assert_eq!(Position(5.0, 5.0), *pos);
-		assert_eq!(Velocity(8.0, 0.0), *vel);
+		if let Some((name, pos, vel)) = ecs.get(&e1)
+		{
+			assert_eq!(Name("Pilot Pete"), *name);
+			assert_eq!(Position(5.0, 5.0), *pos);
+			assert_eq!(Velocity(8.0, 0.0), *vel);
+		}
 
-		let (name, pos, vel) = ecs.get(&e2);
-		assert_eq!(Name("Tame Impala"), *name);
-		assert_eq!(Position(0.1, -50.0), *pos);
-		assert_eq!(Velocity(0.0, -10.0), *vel);
+		if let Some((name, pos, vel)) = ecs.get(&e2)
+		{
+			assert_eq!(Name("Tame Impala"), *name);
+			assert_eq!(Position(0.1, -50.0), *pos);
+			assert_eq!(Velocity(0.0, -10.0), *vel);
+		}
+	}
+
+	#[test]
+	fn remove_entity()
+	{
+		let mut ecs = EntityComponentSystem::new();
+		let mut e1 = ecs.create_entity(
+			"Pilot Pete",
+			Position(5.0, 5.0),
+			Velocity(8.0, 0.0),
+			);
+		let e2 = ecs.create_entity(
+			"Tame Impala",
+			Position(0.1, -50.0),
+			Velocity(0.0, -10.0),
+			);
+
+		ecs.remove_entity(&mut e1);
+		assert_eq!(None, ecs.get(&e1));
+
+		if let Some((name, pos, vel)) = ecs.get(&e2)
+		{
+			assert_eq!(Name("Tame Impala"), *name);
+			assert_eq!(Position(0.1, -50.0), *pos);
+			assert_eq!(Velocity(0.0, -10.0), *vel);
+		}
 	}
 }
