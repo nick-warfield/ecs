a55a50f90fa53b57789fddcdfdf64b89c392f069
Nicholas Warfield <warfield9000@gmail.com>
2020-01-08 20:42:18 -0800

    Entry Refactor Refactored code to reference by a custom entry type instead of option. Makes it much easier to track generation, and tracks if the entity is alive.

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,69 @@
 // just for testing
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Default, PartialEq)]
 pub struct Name<'a>(&'a str);
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Default, PartialEq)]
 pub struct Position(f32, f32);
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Default, PartialEq)]
 pub struct Velocity(f32, f32);
 
-// this is just a generational index?
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct Entity
 {
-	pub generation: usize,
-	pub index: usize,
+	generation: usize,
+	index: usize,
 }
 
+#[derive(Debug, Copy, Clone)]
+pub struct Entry<T: Default>
+{
+	value: T,
+	generation: usize,
+}
+impl<T: Default> Entry<T>
+{
+	pub fn new(value: T, generation: usize) -> Entry<T>
+	{
+		Entry {
+			value,
+			generation,
+		}
+	}
+
+	pub fn as_ref(&self) -> Option<&T>
+	{
+		if self.is_alive() { None }
+		else { Some(&self.value) }
+	}
+	pub fn as_mut(&mut self) -> Option<&mut T>
+	{
+		if self.is_alive() { None }
+		else { Some(&mut self.value) }
+	}
+
+	pub fn is_alive(&self) -> bool { self.generation == 0 }
+	pub fn generation(&self) -> usize { self.generation }
+	pub fn kill(&mut self) -> Option<T>
+	{
+		if self.is_alive()
+		{
+			self.generation = 0;
+			Some(std::mem::take(&mut self.value))
+		}
+		else
+		{
+			None
+		}
+	}
+}
 
 // name this better, it's accurate but long
 #[derive(Debug)]
 pub struct EntityComponentSystem<'a>
 {
-	generation: usize,
-	names: Vec<Name<'a>>,
-	positions: Vec<Position>,
-	velocities: Vec<Velocity>,
+	next_allocation: Entity,
+	names: Vec<Entry<Name<'a>>>,
+	positions: Vec<Entry<Position>>,
+	velocities: Vec<Entry<Velocity>>,
 }
 
 impl<'a> EntityComponentSystem<'a>
@@ -30,7 +71,7 @@ impl<'a> EntityComponentSystem<'a>
 	pub fn new() -> EntityComponentSystem<'a>
 	{
 		EntityComponentSystem {
-			generation: 1,
+			next_allocation: Entity { index: 0, generation: 0 },
 			names: vec![],
 			positions: vec![],
 			velocities: vec![],
@@ -43,33 +84,48 @@ impl<'a> EntityComponentSystem<'a>
 		velocity: Velocity,
 		) -> Entity
 	{
-		self.names.push(Name(name));
-		self.positions.push(position);
-		self.velocities.push(velocity);
-		Entity {
-			generation: self.generation,
-			index: self.names.len() - 1
+		let next = self.next_allocation.index;
+		let gen = self.next_allocation.generation;
+		if next == self.names.len()
+		{
+			self.names.push(Entry::new(Name(name), gen));
+			self.positions.push(Entry::new(position, gen));
+			self.velocities.push(Entry::new(velocity, gen));
 		}
+		else
+		{
+			self.names[next] = Entry::new(Name(name), gen);
+			self.positions[next] = Entry::new(position, gen);
+			self.velocities[next] = Entry::new(velocity, gen);
+		}
+
+		let entity = self.next_allocation;
+		self.next_allocation.index += 1;
+		entity
 	}
 
-	pub fn remove_entity(&mut self, ent: &mut Entity)
+	pub fn remove_entity(&mut self, ent: &Entity)
 	{
-		ent.generation = 0;
+		self.names[ent.index].kill();
+		self.positions[ent.index].kill();
+		self.velocities[ent.index].kill();
+		self.next_allocation.index = ent.index;
 	}
 
 	pub fn get(&self, ent: &Entity)
 		-> Option<(&'a Name, &'a Position, &'a Velocity)>
 	{
-		if ent.generation != self.generation
+		if let (Some(name), Some(pos), Some(vel)) = (
+			self.names[ent.index].as_ref(),
+			self.positions[ent.index].as_ref(),
+			self.velocities[ent.index].as_ref(),
+			)
 		{
-			None
+			Some((name, pos, vel))
 		}
 		else
 		{
-			Some((&self.names[ent.index],
-			 &self.positions[ent.index],
-			 &self.velocities[ent.index],
-			 ))
+			None
 		}
 	}
 }
@@ -113,7 +169,7 @@ pub mod tests
 	fn remove_entity()
 	{
 		let mut ecs = EntityComponentSystem::new();
-		let mut e1 = ecs.create_entity(
+		let e1 = ecs.create_entity(
 			"Pilot Pete",
 			Position(5.0, 5.0),
 			Velocity(8.0, 0.0),
@@ -124,7 +180,7 @@ pub mod tests
 			Velocity(0.0, -10.0),
 			);
 
-		ecs.remove_entity(&mut e1);
+		ecs.remove_entity(&e1);
 		assert_eq!(None, ecs.get(&e1));
 
 		if let Some((name, pos, vel)) = ecs.get(&e2)
